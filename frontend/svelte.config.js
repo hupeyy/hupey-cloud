e2df74a2f0ce6dcde8dfd856b2aa878ab12d824b
hupeyy <huperw@ufl.edu>
2025-04-03 19:08:50 -0400

    serving on nginx

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,12 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import autoAdapter from '@sveltejs/adapter-auto';
+import nodeAdapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const isNode = process.env.ADAPTER === 'node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: isNode ? nodeAdapter({ out: 'build' }) : autoAdapter()
 	}
 };
 
